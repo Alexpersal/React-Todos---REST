84f49c141c686fc253741a04c2ceb8e6bb2df37e
Alexperezsal <al.pes79@gmail.com>
2022-03-24 18:51:39 +0100

    React_Todo_Rest

--- src/todos/index.js
+++ src/todos/index.js
@@ -1,41 +1,46 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import { AfegirTodo } from "./AfegirTodo";
-import { TodoItem } from "./TodoItem";
+import { TodoList } from "./TodoList";
 import { getTodos } from "./todosApi";
 
+const initialState = [];
+
+function reducerTodos(state = initialState, action) {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [...state, action.todo];
+    case "UPDATE_TODO":
+      return state.map((currentTodo) =>
+        currentTodo.id === action.todo.id ? action.todo : currentTodo
+      );
+    default:
+      return state;
+  }
+}
 export function Todos() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(initialState);
+  const loadTodos = () => getTodos().then((allTodos) => setTodos(allTodos));
 
   useEffect(() => {
-    getTodos().then(setTodos);
+    loadTodos();
 
-    const intervalID = setInterval(() => {
-      getTodos().then(setTodos);
-    }, 6000);
+    const intervalID = setInterval(() => loadTodos, 6000);
     return () => clearInterval(intervalID);
   }, []);
+
+  const onTodoAdded = (todo) =>
+    setTodos(reducerTodos(todos, { type: "ADD_TODO", todo }));
+
+  const onTodoUpdated = (updatedTodo) =>
+    setTodos(reducerTodos(todos, { type: "UPDATE_TODO", todo: updatedTodo }));
 
   return (
     <div className="App">
       <h1>Llista de TODOS</h1>
-      <button onClick={() => getTodos().then(setTodos)}>Refresh</button>
-      <ul>
-        {todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            onUpdated={(updatedTodo) =>
-              setTodos(
-                todos.map((currentTodo) =>
-                  currentTodo.id === updatedTodo.id ? updatedTodo : currentTodo
-                )
-              )
-            }
-          />
-        ))}
-      </ul>
-      <AfegirTodo OnTodoAdded={(todo) => setTodos([...todos, todo])} />
+      <button onClick={loadTodos}>Refresh</button>
+      <TodoList todos={todos} onUpdated={onTodoUpdated} />
+      <AfegirTodo OnTodoAdded={onTodoAdded} />
     </div>
   );
 }
